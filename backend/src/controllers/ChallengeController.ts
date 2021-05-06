96c54d2d4276c70ae137d829cfc96ab5831e5f76
javierluque96 <javier.lr96@gmail.com>
2021-05-06 15:01:50 +0200

    get all and create

--- backend/src/controllers/ChallengeController.ts
+++ backend/src/controllers/ChallengeController.ts
@@ -1,12 +1,32 @@
+import { validate } from "class-validator";
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { env } from "../environment";
+import EntityHelper from "../libs/EntityHelper";
+import { Challenge } from "../models/Challenge";
 
 export default class ChallengeController {
   static async create(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json({ status: "success" });
+    try {
+      const challenge = new Challenge();
+      EntityHelper.setColumnsFromBody(challenge, req.body);
+
+      const errors = await validate(challenge, env.validationOptions);
+      if (errors.length > 0) return res.status(400).json(errors);
+
+      await getRepository(Challenge).save(challenge);
+      return res.status(200).json({ status: "success" });
+    } catch (err) {
+      return res.status(400).json({ status: "error" });
+    }
   }
 
   static async getAll(req: Request, res: Response): Promise<Response> {
-    return res.status(200).json({ status: "success" });
+    try {
+      return res.json(await getRepository(Challenge).find());
+    } catch (err) {
+      return res.status(400).json({ status: "error" });
+    }
   }
 
   static async getOneById(req: Request, res: Response): Promise<Response> {
