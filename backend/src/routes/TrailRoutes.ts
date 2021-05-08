67122d153beca501a7858abe2d3e8050de4ec451
javier.luque96 <javier.lr96@gmail.com>
2021-05-08 13:48:36 +0200

    bug 13 file exists

--- backend/src/routes/TrailRoutes.ts
+++ backend/src/routes/TrailRoutes.ts
@@ -2,15 +2,15 @@ import { Router } from "express";
 import TrailController from "../controllers/TrailController";
 import StorageHelper from "../libs/StorageHelper";
 import { verifyToken } from "../middlewares/auth";
-import { checkFileExtension } from "../middlewares/trail";
+import { checkFile } from "../middlewares/trail";
 
 const router = Router();
 const multer = StorageHelper.multer;
 
 router.get("/getAll", [verifyToken], TrailController.getAll);
 router.get("/getOneById", [verifyToken], TrailController.getOneById);
-router.post("/create", [verifyToken, checkFileExtension], TrailController.create);
-router.put("/update", [verifyToken, checkFileExtension], TrailController.update);
+router.post("/create", [verifyToken, checkFile], TrailController.create);
+router.put("/update", [verifyToken, checkFile], TrailController.update);
 router.delete("/delete", [verifyToken], TrailController.delete);
 router.post("/upload", [verifyToken, multer.single("file0")], TrailController.uploadFile);
 router.get("/getTrackFile", [verifyToken], TrailController.getTrackFile);
