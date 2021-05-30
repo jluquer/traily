145b0dfa158cbe47652f965a09569dc32592e496
javier.luque96 <javier.lr96@gmail.com>
2021-05-30 23:02:30 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,13 @@ import * as helmet from "helmet";
 import * as morgan from "morgan";
 import * as cors from "cors";
 const port = process.env.PORT || 3000;
-const allowedOrigins = ["http://localhost:4200", "https://jluquer.github.io"];
-const corsConfig = cors({
-  credentials: true,
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error(`Origin: ${origin} is now allowed`));
-    }
-  },
-});
 
 createConnection()
   .then(async () => {
     const app = express();
 
-    app.options("*", corsConfig);
-    app.use(corsConfig);
+    app.options("*", cors());
+    app.use(cors());
     app.use(helmet());
     app.use(morgan("dev"));
     app.use(express.json());
