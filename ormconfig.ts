cca6eac3e8291c89f69e737d5f7d15c5f18a40d8
javier.luque96 <javier.lr96@gmail.com>
2021-05-31 16:54:43 +0200

    heroku db

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,11 +2,11 @@ const prod = process.argv.includes("--prod") || false;
 
 export default {
   type: "mysql",
-  host: prod ? "remotemysql.com" : "localhost",
+  host: prod ? "us-cdbr-east-04.cleardb.com" : "localhost",
   port: 3306,
-  username: prod ? "mb1GeoCx3C" : "luque",
-  password: prod ? "nMB7Jkfi9i" : "luque",
-  database: prod ? "mb1GeoCx3C" : "traily",
+  username: prod ? "bfe3318d5ddf4b" : "luque",
+  password: prod ? "3faaed65" : "luque",
+  database: prod ? "heroku_ce9defb7073d5b7" : "traily",
   synchronize: true,
   logging: false,
   entities: ["src/models/**/*.ts"],
