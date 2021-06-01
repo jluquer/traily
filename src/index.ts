3b393cfb613795fda5382a42a2c5b106e947adf7
javier.luque96 <javier.lr96@gmail.com>
2021-06-01 19:58:59 +0200

    prod

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,18 @@ import * as morgan from "morgan";
 import * as cors from "cors";
 const port = process.env.PORT || 3000;
 
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api", routes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
 createConnection()
-  .then(async () => {
-    const app = express();
-
-    app.use(cors());
-    app.use(helmet());
-    app.use(morgan("dev"));
-    app.use(express.json());
-    app.use("/api", routes);
-
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
-    });
-  })
+  .then()
   .catch((error) => console.log(error));
